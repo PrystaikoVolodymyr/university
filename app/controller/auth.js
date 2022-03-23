ec46ff0094bc089e49847ffb4ee60767be9e7cd8
Volodymyr Prystaiko <volodymyr.prystaiko@bitcom.systems>
2022-03-24 00:21:08 +0200

    add-twilio-auth

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('../crypto/bcrypt');
+const TwoFactorAuth = require('../models/twoFactorAuth')
 
 module.exports = {
     async singUp(req, res) {
@@ -26,17 +27,30 @@ module.exports = {
     async singIn(req, res) {
         try {
             const { email, password } = req.body;
-            const candidate = await User.findOne({ email });
+            const user = await User.findOne({ email });
+            const code = Math.floor(1000 + Math.random() * 9000);
 
-            if (!candidate) {
+
+            if (!user) {
                 return res.status(400).json({ message: 'Wrong email or password' });
             }
 
-            await bcrypt.compare(password, candidate.password);
+            await bcrypt.compare(password, user.password);
+
+            await TwoFactorAuth.create({
+                phone: user.phone,
+                code: code,
+            })
 
             res.status(201).json({ message: 'Login is complete' });
         } catch (e) {
             res.status(500).json(e.message);
         }
+    },
+
+    async verifyTwoFactorAuth(req, res) {
+        const { email, code } = req.body;
+
+
     }
 };
--- app/router/auth.js
+++ app/router/auth.js
@@ -1,10 +1,12 @@
 // rout => auth/
 
 const router = require('express').Router();
-const { authController } = require('../controller');
+const { authController, userController} = require('../controller');
 const { userMiddleware } = require('../middleware');
 
 router.post('/sing-up', userMiddleware.createUser, authController.singUp);
 router.post('/sing-in', userMiddleware.singIn, authController.singIn);
+router.post('/two-factor-authenticate', );
+
 
 module.exports = router;
